eb05fbece5c793c66d8e9ef034ee018e2640b016
albrimpaqarizi <albrimpa@gmail.com>
2019-04-10 01:39:44 +0200

    Create Footer and Header

---
+++ cilent/src/components/Cart.vue
@@ -0,0 +1,20 @@
+<template>
+<h1>hello</h1>
+</template>
+
+<script>
+export default {
+  name: 'Cart',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
